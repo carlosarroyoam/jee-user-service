6a24328bfc0c0d69ba86abbd7fbbb2c3e7055c31
Carlos Alberto Arroyo Martínez <43684710+carlosarroyoam@users.noreply.github.com>
2024-02-26 19:54:26 -0600

    Update import.sql

--- src/main/resources/META-INF/import.sql
+++ src/main/resources/META-INF/import.sql
@@ -1,5 +1,4 @@
 INSERT INTO roles(id, title, description) VALUES(1, 'Admin', 'Role for admins users');
-	
 INSERT INTO roles(id, title, description) VALUES(2, 'User', 'Role for normal users');
 
 INSERT INTO users(id, name, age, username, email, password, role_id, is_active, created_at, updated_at) VALUES(1, 'Carlos Alberto Arroyo Martínez', 28, 'carroyom', 'carroyom@mail.com', '$2a$10$eAksNP3QN8numBgJwshVpOg2ywD5o6YxOW/4WCrk/dZmV77pC6QqC', 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
